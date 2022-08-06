eeac95c40e8ba41369aaa97fd43b9134886e1ca3
vdevcode <thanhvumaidao1234567@gmail.com>
2022-08-06 23:44:59 +0700

    foist

--- src/index.js
+++ src/index.js
@@ -1,14 +1,96 @@
+const buttonAllow = document.querySelector('.buttons')
+const showTextButtons = document.querySelector('.buttons button')
+buttonAllow.addEventListener('click',()=>{
+   
+        showTextButtons.innerText = 'Đang Play...'
+        if(music.play()){
+            if(showTextButtons.innerText = 'Đang Play...'){
+                playMusic.classList.add('bx-pause')
+                playMusic.classList.remove('bx-play')
+                posterPlay.classList.add('active')
+                buttonAllow.addEventListener('click',()=>{
+                    showTextButtons.innerText = 'PLAY'
+                    playMusic.classList.remove('bx-pause')
+                    playMusic.classList.add('bx-play')
+                    posterPlay.classList.remove('active')
+                    music.pause()
+                })
+            }
+            music.play()
+        }
+        else{
+            if(music.pause()){
+            if(showTextButtons.classList.contains = 'PLAY'){
+                buttonAllow.addEventListener('click',()=>{
+                    showTextButtons.innerText = 'Đang Play...'
+                    playMusic.classList.add('bx-pause')
+                    playMusic.classList.remove('bx-play')
+                    posterPlay.classList.add('active')
+                    music.play()
+                })
+            }
+            music.pause()
+        }}
 
+        playMusic.addEventListener('click',()=>{
+           if(playMusic.classList.contains('bx-play')){       
+               posterPlay.classList.remove('active')
+            }
+            else{
+               showTextButtons.innerText = 'Đang Play...'
+               
+            }
+        })       
+})
+
+// buttonAllow.addEventListener('click',()=>{
+//     showTextButtons.innerText = 'Đang Play...'
+//     ClickPauseSong()
+//     ClickPlaySong()
+// })
+
+// function ClickPauseSong(){
+//     if(showTextButtons.classList.contains = 'Đang Play...'){
+//         buttonAllow.addEventListener('click',()=>{
+//             showTextButtons.innerText = 'Play'
+//             playMusic.classList.add('bx-play')
+//             posterPlay.classList.remove('active')
+//             posterPlay.classList.remove('active')
+//             music.play()
+//         })   
+//     }
+// //    ClickPlaySong()
+
+// }
+
+// function ClickPlaySong(){
+//     if(showTextButtons.classList.contains = 'Play'){
+//         buttonAllow.addEventListener('click',()=>{
+//                  showTextButtons.innerText = 'Đang Play...'
+//                  posterPlay.classList.add('active')
+//                  playMusic.classList.add('bx-pause')
+//                   playMusic.classList.remove('bx-play')
+//                  music.pause()
+//         })
+//      }
+// }
 
 
 //open menu
 let openMenu = document.querySelector('.bxs-grid')
 let showMenu = document.querySelector('.menu-side')
 
+
 openMenu.addEventListener('click',function(){
     showMenu.classList.toggle('active')
-    document.body.classList.remove('active')
+   
 })
+
+// const buttonPlaySong = document.querySelector('.bxs-caret-right-circle')
+// buttonPlaySong.addEventListener('click',function(){
+//     buttonPlaySong.classList.remove('bxs-caret-right-circle')
+//     buttonPlaySong.classList.add('bx-stop-circle')
+// })
 
 //close menu
 let closeMenu = document.querySelector('.bx-x')
@@ -152,18 +234,60 @@ const Songs = [
         <div class="sub__author">Lee Ken x Nal</div>`,
         poster: './image/18.jpg',
     },
-    // {
-    //     id: 19,
-    //     songName: `On My Way<br>
-    //     <div class="sub__author">Alan Walker</div>`,
-    //     poster: './image/19.jpg',
-    // },
-    // {
-    //     id: 20,
-    //     songName: `On My Way<br>
-    //     <div class="sub__author">Alan Walker</div>`,
-    //     poster: './image/20.jpg',
-    // }
+    {
+        id: 19,
+        songName: `Chúc tết<br>
+        <div class="sub__author">Khởi My</div>`,
+        poster: './image/19.jpg',
+    },
+    {
+        id: 20,
+        songName: `ngày xuân lông phụng sum vầy<br>
+        <div class="sub__author">Lam Trường</div>`,
+        poster: './image/20.jpg',
+    },
+    {
+        id: 21,
+        songName: `tình yêu đơn phương là gì<br>
+        <div class="sub__author">NC</div>`,
+        poster: './image/21.jpg',
+    },
+    {
+        id: 22,
+        songName: `quá khứ anh không thể quên<br>
+        <div class="sub__author">Dương Minh Tuấn</div>`,
+        poster: './image/22.jpg',
+    },
+     {
+        id: 23,
+        songName: `em ơi anh phải làm sao<br>
+        <div class="sub__author">Dương Minh Tuấn</div>`,
+        poster: './image/23.jpg',
+    },
+     {
+        id: 24,
+        songName: `anh yêu vội thế<br>
+        <div class="sub__author">Mee Remix</div>`,
+        poster: './image/24.jpg',
+    },
+     {
+        id: 25,
+        songName: `tướng quân<br>
+        <div class="sub__author">Nhật Phong</div>`,
+        poster: './image/25.jpg',
+    },
+    {
+        id: 26,
+        songName: `thời gian sẽ trả lời<br>
+        <div class="sub__author">Kaiz</div>`,
+        poster: './image/26.jpg',
+    },
+    {
+        id: 27,
+        songName: `phai dấu cuộc tình<br>
+        <div class="sub__author">Phạm Kỳ Anh</div>`,
+        poster: './image/26.jpg',
+    },
     
 ]
 
@@ -184,7 +308,7 @@ music.pause()
 
 //play music (master play)
 const playMusic = document.getElementById('playMusic')
-const wave = document.getElementsByClassName('wave')
+
 
 //buttons play music bottom
 playMusic.addEventListener('click', function () {
@@ -192,13 +316,15 @@ playMusic.addEventListener('click', function () {
         music.play()
         playMusic.classList.add('bx-pause')
         playMusic.classList.remove('bx-play')
-
+        posterPlay.classList.add('active')
+        showTextButtons.innerText = 'Đang Play...'
 
     } else {
         music.pause()
         playMusic.classList.add('bx-play')
         playMusic.classList.remove('bx-pause')
-
+        posterPlay.classList.remove('active')
+        showTextButtons.innerText = 'Play'
     }
 })
 
@@ -226,6 +352,7 @@ let index = 0
 let playSongs = document.getElementsByClassName('playIconSong')
 const posterPlay = document.getElementById('poster_master_play')
 const titleSong = document.getElementById('title')
+const downloadMusic = document.getElementById('download__Song')
 
 Array.from(playSongs).forEach((e, i) => {
     e.addEventListener('click', (event) => {
@@ -235,6 +362,9 @@ Array.from(playSongs).forEach((e, i) => {
         // titleSong.innerHTML = Songs[i].songName
         playMusic.classList.add('bx-pause')
         playMusic.classList.remove('bx-play')
+        showTextButtons.innerText = 'Đang Play...'
+        posterPlay.classList.add('active')
+        downloadMusic.href = `audio/${index}.mp3`
         music.play()
 
         let songTitles = Songs.filter((els) => {
@@ -246,6 +376,7 @@ Array.from(playSongs).forEach((e, i) => {
             let { songName, poster } = elss
             titleSong.innerHTML = songName
             posterPlay.src = poster
+            downloadMusic.setAttribute('download',songName) 
         })
 
         makeAllBackground()
@@ -337,10 +468,11 @@ let nextMusic = document.getElementById('next')
 let downMusic = document.getElementById('download')
 
 backMusic.addEventListener('click',function(){
-    index++
-    // if(index < 1){
-    //     index = Array.from(backgroundSong).length
-    // }
+    index-=1
+    if(index <1){
+        index = 1
+        
+    }
     music.src = `./audio/${index}.mp3`
     // posterPlay.src = `./image/${index}.jpg`
     // titleSong.innerHTML = Songs[i].songName
@@ -366,9 +498,10 @@ backMusic.addEventListener('click',function(){
 })
 
 nextMusic.addEventListener('click',function(){
-    index += 1
-    if(index > 20){
+    index++
+    if(index > Array.from(backgroundSong).length){
         index = Array.from(backgroundSong).length
+        alert('bài cuối r đó,đừng ấn qua bài tiếp theo nữa')
     }
     music.src = `./audio/${index}.mp3`
     // posterPlay.src = `./image/${index}.jpg`
