8edf7ffb62a57b1c96a85158fdab132b27fa100e
vdevcode <thanhvumaidao1234567@gmail.com>
2022-12-10 12:52:09 +0700

    ipload source

--- src/index.js
+++ src/index.js
@@ -427,6 +427,13 @@ const Songs = [
         <div class="sub__author">RPT Orijinn x Ronboogz</div>`,
         poster: './image/52.jpg',
     },
+    {
+        id: 54,
+        songName: `Don't côi<br>
+        <div class="sub__author">RPT Orijinn x Ronboogz</div>`,
+        poster: './image/52.jpg',
+    },
+
 
 ]
 
@@ -1084,6 +1091,16 @@ showNoel1.addEventListener("click",() => {
     SnowDelay()
 
 })
+const showNoel2 = document.querySelector("#backgroundNoel2")
+
+
+showNoel2.addEventListener("click",() => {
+    var objectSnow = {
+        func: SnowDelay()
+    }
+    delete objectSnow.func
+})
+
 
 const popupScreenShow = document.querySelector('.popupScreen');
 const closePopupHidden = document.querySelector(".close--Popup");
@@ -1091,6 +1108,7 @@ const ShowHiddenItemPopup = document.querySelector("#Popup");
 
 function openPopupShow(){
     popupScreenShow.classList.add("active")
+    ShowHiddenItemPopup.style.transition = ".5s";
 }
 
 ShowHiddenItemPopup.addEventListener("click",openPopupShow)
@@ -1098,3 +1116,52 @@ ShowHiddenItemPopup.addEventListener("click",openPopupShow)
 closePopupHidden.addEventListener("click",function(){
     popupScreenShow.classList.remove("active")
 })
+
+
+//online or ofline
+
+// Selecting all required elements
+const wrapper = document.querySelector(".wrapper"),
+toast = wrapper.querySelector(".toast"),
+title = toast.querySelector("span"),
+subTitle = toast.querySelector("p"),
+wifiIcon = toast.querySelector(".icon"),
+closeIcon = toast.querySelector(".close-icon");
+window.onload = ()=>{
+    function ajax(){
+        let xhr = new XMLHttpRequest(); //creating new XML object
+        xhr.open("GET", "https://jsonplaceholder.typicode.com/posts", true); //sending get request on this URL
+        xhr.onload = ()=>{ //once ajax loaded
+            //if ajax status is equal to 200 or less than 300 that mean user is getting data from that provided url
+            //or his/her response status is 200 that means he/she is online
+            if(xhr.status == 200 && xhr.status < 300){
+                toast.classList.remove("offline");
+                title.innerText = "bạn đang online";
+                subTitle.innerText = "Wifi đã được kết nối !";
+                wifiIcon.innerHTML = '<i class="bx bx-wifi" ></i>';
+                closeIcon.onclick = ()=>{ //hide toast notification on close icon click
+                    wrapper.classList.add("hide");
+                }
+                setTimeout(()=>{ //hide the toast notification automatically after 5 seconds
+                    wrapper.classList.add("hide");
+                }, 5000);
+            }else{
+                offline(); //calling offline function if ajax status is not equal to 200 or not less that 300
+            }
+        }
+        xhr.onerror = ()=>{
+            offline(); ////calling offline function if the passed url is not correct or returning 404 or other error
+        }
+        xhr.send(); //sending get request to the passed url
+    }
+    function offline(){ //function for offline
+        wrapper.classList.remove("hide");
+        toast.classList.add("offline");
+        title.innerText = "Bạn đang offline";
+        subTitle.innerText = "Bạn đã mất kết nối wifi";
+        wifiIcon.innerHTML = '<i class="bx bx-wifi-off" ></i>';
+    }
+    setInterval(()=>{ //this setInterval function call ajax frequently after 100ms
+        ajax();
+    }, 100);
+}
